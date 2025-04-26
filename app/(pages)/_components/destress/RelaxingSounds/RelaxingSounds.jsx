654830a1c15090764eb151219c52ee1ca5603d27
munneth <mugill@ucdavis.edu>
2025-04-25 19:27:27 -0700

    init commit

--- app/(pages)/_components/destress/RelaxingSounds/RelaxingSounds.jsx
+++ app/(pages)/_components/destress/RelaxingSounds/RelaxingSounds.jsx
@@ -1,6 +1,11 @@
-export default function RelaxingSounds() {
+import Image from "next/image";
+
+function RelaxingSounds() {
     return (
         <div>
+            <h1>Relaxing Sounds</h1>
         </div>
     )
 }
+export default RelaxingSounds;
+
--- app/(pages)/resources/page.jsx
+++ app/(pages)/resources/page.jsx
@@ -1,10 +1,19 @@
+import RelaxingSounds from "../_components/destress/RelaxingSounds/RelaxingSounds.jsx";
 import styles from "./page.module.scss";
 
 export default function Home() {
   return (
-    <main className={styles.page}>
-      <h1>header 1</h1>
-      <p>body</p>
-    </main>
+    <>
+      <main className={styles.page}>
+        <h1>hader 1</h1>
+        <p>body</p>
+        <h1>Relaxing Sounds</h1>
+        <RelaxingSounds />
+      </main>
+      <div>
+        <h1>Relaxing Sounds</h1>
+        <RelaxingSounds />
+      </div>
+    </>
   );
 }

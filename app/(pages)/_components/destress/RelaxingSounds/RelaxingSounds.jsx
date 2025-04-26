b0117575998b0a4b8d41ba6830d9909bb7489e07
munneth <mugill@ucdavis.edu>
2025-04-26 12:09:07 -0700

    made scroll feature still needs some work but works for the most part

--- app/(pages)/_components/destress/RelaxingSounds/RelaxingSounds.jsx
+++ app/(pages)/_components/destress/RelaxingSounds/RelaxingSounds.jsx
@@ -1,27 +1,69 @@
+
+"use client";
 import Image from "next/image";
 import styles from "./RelaxingSounds.module.scss";
-import MusicCard from "./musicCard";            
+import MusicCard from "./musicCard"; 
+import { useRef } from "react"; 
+
 
 function RelaxingSounds() {
+    const containerRef = useRef(null);
+    const scrollNext = () => {
+        if(containerRef.current){
+            const container = containerRef.current;
+            const cardWidth = container.firstChild.offsetWidth;
+            container.scrollBy({ left: cardWidth * 3, behavior: "smooth" });
+        }
+    }
+    const scrollPrev = () => {
+        if (containerRef.current) {
+            const container = containerRef.current;
+            const cardWidth = container.firstChild.offsetWidth;
+            container.scrollBy({ left: -cardWidth * 3, behavior: "smooth" });
+        }
+    };
     return (
         <div className={styles.background}>
             <div>
                 <h1 className={styles.title}>Relaxing Sounds</h1>
-            </div>
+            </div>          
             <div className={styles.musicCardContainer}>
+                <Image
+                    onClick={scrollPrev}
+                    src="/images/destress_relaxing_sounds/leftScroll.svg"
+                    alt="scroll left"
+                    width={281}
+                    height={387}
+                    className={styles.scrollButtons}
+                />
+            <div ref={containerRef} className={styles.cardsTrack}>
                 <MusicCard 
                     imagePath = "rain"
                     altText = "Rain sound"
-                    />
+                />
                 <MusicCard
                     imagePath = "piano"
                     altText = "piano sound"
-                    />
+                />
                 <MusicCard
                     imagePath = "ocean"
                     altText = "ocean sound"
-                    />
+                />
+                <MusicCard 
+                    imagePath = "piano"
+                    altText = "piano sound"
+                />
+                <MusicCard
+                    imagePath = "rain"
+                    altText = "rain sound"
+                />
+                <MusicCard
+                    imagePath = "ocean"
+                    altText = "ocean sound"
+                />
+            </div>
                 <Image
+                    onClick={scrollNext}
                     src="/images/destress_relaxing_sounds/rightScroll.svg"
                     alt="scroll right"
                     width={281}
@@ -30,6 +72,7 @@ function RelaxingSounds() {
                 />
             </div>
         </div>
+        
     )
 }
 export default RelaxingSounds;

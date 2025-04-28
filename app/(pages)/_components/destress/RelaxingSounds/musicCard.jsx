8ab7d01a3e962fa704424b3d8f0d3ba626b31310
munneth <mugill@ucdavis.edu>
2025-04-28 16:39:30 -0700

    added play/pause button as separate element; replaced image with proper image

--- app/(pages)/_components/destress/RelaxingSounds/musicCard.jsx
+++ app/(pages)/_components/destress/RelaxingSounds/musicCard.jsx
@@ -1,16 +1,25 @@
 import Image from "next/image";
+import styles from "./musicCard.module.scss";
 
 export default function RelaxingSounds({ imagePath, altText }) {
     return (
-        <div>
+        <div className={styles.cardContainer}>
             <Image 
             src={`/images/destress_relaxing_sounds/${imagePath}MusicCard.svg`}
             alt={altText} 
             width={281}
             height={387}
+            className={styles.largeImage}
             />
 
-            
+            <Image
+            src={"/images/destress_relaxing_sounds/playbutton.svg"}
+            alt={"play button"} 
+            width={40}
+            height={40}
+            className={styles.overlayImage}
+            />
+
         </div>
     )
 }

64e0455f9abaea0288421be9ee3f6d898771ed58
austin1im <alaustinall@gmail.com>
2025-04-30 22:07:28 -0700

    Created the Resource Card Component
    additional information for styling needed

--- app/(pages)/_components/resources/ResourceCard/ResourceCard.jsx
+++ app/(pages)/_components/resources/ResourceCard/ResourceCard.jsx
@@ -1,6 +1,77 @@
+"use client";
+import Image from "next/image";
+import styles from "./ResourceCard.module.scss";
+import { useState, useEffect, useRef } from 'react'
+
+const testData = {
+    mainTitle: "awesometitle",
+    buttons: [
+        {
+            heading: "coolbutton",
+            text: "Lorem ipsum dolor sit amegna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+            link: "https://www.roblox.com"
+        },
+        {
+            heading: "anothercoolbutton",
+            text: "odsfiekwnfoiwefhoiewfhhioecitation ullamco labori",
+            link: "https://www.youtube.com"
+        }
+    ]
+}
+
+function ButtonWithDropdown({ titleText, dropdownText, link }) {
+    const [clicked, setClicked] = useState(false);
+    const dropdownRef = useRef();
+    useEffect(() => {
+        const handleBackgroundClick = (e) => {
+            if (!dropdownRef.current.contains(e.target)) {
+                setClicked(false);
+            }
+        }
+        document.addEventListener('mousedown', handleBackgroundClick);
+        return () => document.removeEventListener('mousedown', handleBackgroundClick);
+    }, []);
+    const handleLinkClick = (e) => {
+        e.stopPropagation();
+    };
+
+    return (
+        <div className={`${styles.dropdownButton} ${clicked ? styles.open : ''}`} ref={dropdownRef} onClick={() => setClicked(!clicked)}>
+            <div className={styles.boxAndSVG}>
+                <div className={styles.dropdownButtonBox}>
+                    {titleText}
+                </div>
+                <Image className={`${styles.dropdownSVG} ${clicked ? styles.rotated : ''}`} src='/images/gridicons:dropdown.svg' width={62} height={62} alt="gridicons:dropdown" draggable="false" />
+            </div>
+            {clicked && (
+                <div>
+                    <p className={styles.expandedText}>
+                        {dropdownText}
+                    </p>
+                    <p className={styles.expandedText}>
+                        Link: <a href={link} onClick={handleLinkClick}>{link}</a>
+                    </p>
+                </div>
+            )}
+        </div>
+    )
+}
+
+function Card({ data }) {
+    return (
+        <div className={styles.card}>
+            {data.mainTitle}
+            {data.buttons.map((buttonData, index) => (
+                <ButtonWithDropdown key={index} titleText={buttonData.heading} dropdownText={buttonData.text} link={buttonData.link}/>
+            ))}
+        </div>
+    )
+}
+
 export default function ResourceCard() {
     return (
         <div>
+            <Card data={testData} />
         </div>
     )
 }

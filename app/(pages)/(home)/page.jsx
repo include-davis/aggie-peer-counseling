40956162bd3c9944847c0e4e2e8df8fdc283b8b3
Anik Majumdar <amaj@ucdavis.edu>
2025-05-19 21:05:30 -0700

    working on the components

--- app/(pages)/(home)/page.jsx
+++ app/(pages)/(home)/page.jsx
@@ -1,10 +1,18 @@
+import QuoteBanner from "../_components/destress/QuoteBanner/QuoteBanner.jsx";
 import styles from "./page.module.scss";
 
 export default function Home() {
   return (
+    <>
     <main className={styles.page}>
-      <h1>header 1</h1>
-      <p>body</p>
+      <div>
+      {/* Banner 1 - TOP */}
+      <QuoteBanner bannerNumber={1} />
+
+      {/* Banner 2 - BOTTOM */}
+      <QuoteBanner bannerNumber={2} />
+    </div>
     </main>
+    </>
   );
 }
---
+++ app/(pages)/_components/destress/QuoteBanner/QuoteBanner.jsx
@@ -0,0 +1,53 @@
+// QuoteBanner.jsx
+import React from "react";
+import styles from "./QuoteBanner.module.scss";
+
+const QuoteBanner = ({ bannerNumber }) => {
+  const isBanner1 = bannerNumber === 1;
+
+  const basePath = isBanner1
+    ? "/images/ExercisePageBanner1/ExercisePageBanner1"
+    : "/images/ExercisePageBanner2/ExercisePageBanner2";
+
+  const vectors = [1, 2, 3, 4, 5].map(
+    (num) => `${basePath}/vector${num}.png`
+  );
+
+  return (
+    <div className={styles.banner}>
+      <img
+        src={`${basePath}/background.png`}
+        alt="background"
+        className={styles.background}
+      />
+      {isBanner1 ? (
+        <img
+          src={`${basePath}/stars.png`}
+          alt="stars"
+          className={styles.stars}
+        />
+      ) : (
+        <img
+          src={`${basePath}/star.png`}
+          alt="star"
+          className={styles.singleStar}
+        />
+      )}
+      <img
+        src={`${basePath}/text.png`}
+        alt="text"
+        className={styles.text}
+      />
+      {vectors.map((src, index) => (
+        <img
+          key={index}
+          src={src}
+          alt={`vector${index + 1}`}
+          className={`${styles.vector} ${styles[`vector${index + 1}`]}`}
+        />
+      ))}
+    </div>
+  );
+};
+
+export default QuoteBanner;

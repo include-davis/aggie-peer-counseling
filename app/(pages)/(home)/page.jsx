65a916a8833f9a2e4a31bcae54905752c325a322
kyharac <kyhara.crespin@gmail.com>
2025-05-23 13:32:49 -0700

    initial command

--- app/(pages)/(home)/page.jsx
+++ app/(pages)/(home)/page.jsx
@@ -1,10 +1,11 @@
 import styles from "./page.module.scss";
-
+import Footer from "../_components/common/Footer/Footer"
 export default function Home() {
   return (
     <main className={styles.page}>
       <h1>header 1</h1>
       <p>body</p>
+      <Footer />
     </main>
   );
 }
--- app/(pages)/_components/common/Footer/Footer.jsx
+++ app/(pages)/_components/common/Footer/Footer.jsx
@@ -1,6 +1,103 @@
-export default function Footer() {
+"use client"
+import styles from "./Footer.module.scss"; // Importing CSS module for styling
+import React from "react"; // Importing React library
+import {FaRegCopyright} from "react-icons/fa6"; // Importing icon from react-icons library
+import { IoMail } from "react-icons/io5"; // Importing icon from react-icons library
+import { FaInstagram } from "react-icons/fa"; // Importing icon from react-icons library
+
+
+export default function Footer({variant = "Home"}) {
+
+    let footerBg = styles.HomeFooter; // Default background (Home & Contact)
+    let footerImg = "/HomeBg.png"; // Default image (Home & Contact)
+
+    if (variant == "Resources") {
+        footerBg = styles.ResourcesFooter;
+        footerImg = "/ResourcesBg.png";
+    } else if (variant === "De-Stress") {
+        footerBg = styles.DeStressFooter;
+        footerImg = "/DeStressBg.png"; 
+    } 
+
+    {/* Array of all sections for the footer, left to right */}
+    const footerSections = [
+        {
+            heading: 'Home',
+            href: 'https://docs.google.com/document/d/1A33hGguKK-yHYc7Mr-kdqZvfAaVtTMvB2YKb7ly4Xlw/edit?usp=sharing',
+            links: [
+                { text: 'Emergency Contact', href: '#' },
+                { text: 'Our Mission', href: '#' },
+                { text: 'What We Do', href: '#' },
+                { text: 'Mentor Mentee Program', href: '#' },
+                { text: 'Meet the Mentors', href: '#' },
+                { text: 'Application Forms', href: '#' }
+            ]
+        },
+
+        {
+            heading: 'Resources',
+            href: 'https://docs.google.com/document/d/1A33hGguKK-yHYc7Mr-kdqZvfAaVtTMvB2YKb7ly4Xlw/edit?usp=sharing',
+            links: [
+                { text: 'On-Campus', href: '#' },
+                { text: 'Partner Units', href: '#' },
+                { text: 'Important Additional Resources', href: '#' },
+                { text: '24/7 Emergency Resources', href: '#' },
+                { text: 'Referring Mentees', href: '#' },
+                { text: 'Academic Resources', href: '#' }
+            ]
+        },
+
+        {
+            heading: 'De-Stress',
+            href: 'https://docs.google.com/document/d/1A33hGguKK-yHYc7Mr-kdqZvfAaVtTMvB2YKb7ly4Xlw/edit?usp=sharing',
+            links: [
+                { text: 'Activities', href: '#' },
+                { text: 'Sounds', href: '#' },
+                { text: 'Yoga Videos', href: '#' }
+            ]
+        }
+
+    ];
+    // Rendering the footer component 
     return (
-        <div>
-        </div>
-    )
+        <footer className={`${styles.FooterContainer} ${footerBg}`} // background
+            style={{backgroundImage: `url(${footerImg})`}}> 
+            <div className={styles.Footer}> 
+            {/* Footer section with links */}
+            {footerSections.map((section,index) => (
+                <div key={index} className={styles.FooterColumn}>
+                    <h2><a href={section.href}>{section.heading}</a></h2>
+                    <ul>
+                        {section.links.map((link,i) => (
+                            <li key={i}>
+                                <a href={link.href}>{link.text}</a>
+                            </li>
+                        ))}
+                    </ul>
+                </div> 
+            ))}
+            </div>
+            <div>
+                {/* Main footer section w/ mission & logo*/}
+                <div className={styles.FooterMain}>
+                    <h1>Aggie Peer Counselling</h1>
+                    <p>A by-student, for-students initiative to create a safe space for conversation, encouragement, and connection.</p>
+                    {/* Not sure if I was supposed to use img or image */}
+                    <img
+                        src='FooterLogo.png'
+                        alt='Aggie Peer Counseling Logo'
+                        className={styles.FooterLogo}
+                    />
+                </div>
+            </div>
+            {/* Footer's Footer */}
+            <div className={styles.FooterBottom}>
+                <p><FaRegCopyright/> 2025 Aggie Peer Counselling</p>
+                <div className={styles.FooterIcons}>
+                    <a href='#'> <IoMail/></a>
+                    <a href='#' target="_blank" rel='noopener noreferrer'> <FaInstagram/></a>
+                </div>
+            </div>
+        </footer>
+    );
 }

5e481b5929449a80ab5591008e331147601999d5
munneth <mugill@ucdavis.edu>
2025-05-24 17:00:17 -0700

    initial

--- app/(pages)/_components/contact/Envelope/Envelope.jsx
+++ app/(pages)/_components/contact/Envelope/Envelope.jsx
@@ -1,6 +1,37 @@
-export default function Envelope() {
-    return (
-        <div>
+import React, { useEffect, useState } from 'react';
+import './Envelope.module.scss';
+
+
+const Envelope = () => {
+  const [open, setOpen] = useState(false);
+
+  useEffect(() => {
+    // Automatically add the flap class after a short delay
+    const timer = setTimeout(() => {
+      setOpen(true);
+    }, 500); // you can tweak this delay to match the animation timing
+
+    return () => clearTimeout(timer);
+  }, []);
+
+  return (
+    <div className="container">
+      <div className={`envelope-wrapper ${open ? 'flap' : ''}`}>
+        <div className="envelope">
+          <div className="letter">
+            <div className="text">
+              <strong>Dear Person.</strong>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Numquam labore omnis minus maiores laboriosam, facere in beatae esse.
+              </p>
+            </div>
+          </div>
         </div>
-    )
-}
+        <div className="heart"></div>
+      </div>
+    </div>
+  );
+};
+
+export default Envelope;
---
+++ app/(pages)/_components/contact/contact.jsx
@@ -0,0 +1,12 @@
+"use client";
+import Envelope from './envelope/Envelope';
+
+function ContactSection() {
+  return (
+    <section id="contact">
+      <Envelope />
+    </section>
+  );
+}
+
+export default ContactSection;

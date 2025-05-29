22ad8502f04a9e351837a8660088c6b084e54421
BenicioJPadilla <beniciopadilla8@gmail.com>
2025-05-28 23:36:57 -0700

    added program

--- app/(pages)/(home)/page.jsx
+++ app/(pages)/(home)/page.jsx
@@ -1,5 +1,6 @@
 import styles from "./page.module.scss";
 import Hero from "../_components/common/Hero/Hero";
+import Program from "../_components/home/Program/Program";
 
 export default function Home() {
   return (
@@ -9,6 +10,7 @@ export default function Home() {
         title="Aggie Peer Counseling"
         subtitle="Here for you, Rain or Shine"
       />
+    <Program />
     <main className={styles.page}>
       <h1>home</h1>
       <p></p>

f8eac0217c9c547b85bcfba03e41ff7a3b088ec9
austin1im <alaustinall@gmail.com>
2025-05-28 23:24:03 -0700

    added image

--- app/(pages)/(home)/page.jsx
+++ app/(pages)/(home)/page.jsx
@@ -1,12 +1,13 @@
 import styles from "./page.module.scss";
 import WhatWeDo from "../_components/home/WhatWeDo/WhatWeDo";
 import EmergencyContact from "../_components/home/EmergencyContact/EmergencyContact";
+import FormsSection from "../_components/home/FormsSection/FormsSection";
 export default function Home() {
   return (
     <main className={styles.page}>
       <WhatWeDo></WhatWeDo>
       <EmergencyContact></EmergencyContact>
-      
+      <FormsSection/>
     </main>
   );
 }
--- app/(pages)/_components/home/FormsSection/FormsSection.jsx
+++ app/(pages)/_components/home/FormsSection/FormsSection.jsx
@@ -1,6 +1,10 @@
+import Image from 'next/image';
+import styles from "./FormsSection.module.scss";
+
 export default function FormsSection() {
     return (
-        <div>
-        </div>
+        <main>
+            <Image width={0} height={0} className={styles.backgroundImage} src="./images/FormsSection/background.svg" alt="hello"/>
+        </main>
     )
 }

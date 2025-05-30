571552275d4ec421d196a03ee2c65c0c8dadd0d8
austin1im <alaustinall@gmail.com>
2025-05-29 23:36:17 -0700

    cloud overlapping + mobile support + stored data in an array

--- app/(pages)/_components/home/FormsSection/FormsSection.jsx
+++ app/(pages)/_components/home/FormsSection/FormsSection.jsx
@@ -2,6 +2,12 @@
 import Image from 'next/image';
 import styles from "./FormsSection.module.scss";
 
+const forms = [
+    {text: "Intake Form", imagePath: "./images/FormsSection/form1.svg", link: "https://youtube.com/"},
+    {text: "Mentor Preference", imagePath: "./images/FormsSection/form2.svg", link: "https://roblox.com/"},
+    {text: "Mentor Applications", imagePath: "./images/FormsSection/form3.svg", link: "https://google.com/"}
+]
+
 function Form ({ text , image , link }) {
     return (
         <main className={styles.form_background} style={{backgroundImage: `url(${image})`}} onClick={() => {window.open(link, 'blank', 'noopener,noreferrer')}}>
@@ -16,10 +22,13 @@ export default function FormsSection() {
     return (
         <main className={styles.forms_section}>
             <div className={styles.wrapper}>
-                <Form text={"Intake Form"} image={'./images/FormsSection/intake_form.svg'} link={'https://roblox.com'}/>
-                <Form text={"Mentor Preference"} image={'./images/FormsSection/mentor_preference.svg'} link={'https://roblox.com'}/>
-                <Form text={"Mentor Applications"} image={'./images/FormsSection/mentor_applications.svg'} link={'https://roblox.com'}/>
+                {forms.map((form, index) => (
+                    <Form key={index} text={form.text} image={form.imagePath} link={form.link}/>
+                ))}
             </div>
+            <Image width={0} height={0} src='./images/FormsSection/cloud_left.svg' alt='left cloud' className={styles.cloud_left}/>
+            <Image width={0} height={0} src='./images/FormsSection/cloud_right.svg' alt='right cloud' className={styles.cloud_right}/>
+            <Image width={0} height={0} src='./images/FormsSection/cloud_mobile.svg' alt='right cloud' className={styles.cloud_mobile}/>
         </main>
     )
 }

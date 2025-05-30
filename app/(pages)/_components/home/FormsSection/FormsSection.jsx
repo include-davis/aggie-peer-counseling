3b2a17fe22864eb454eb15ef4da67a468af87559
austin1im <alaustinall@gmail.com>
2025-05-29 21:50:14 -0700

    finished

--- app/(pages)/_components/home/FormsSection/FormsSection.jsx
+++ app/(pages)/_components/home/FormsSection/FormsSection.jsx
@@ -1,12 +1,13 @@
+"use client";
 import Image from 'next/image';
 import styles from "./FormsSection.module.scss";
 
-function Form ({ text , image }) {
+function Form ({ text , image , link }) {
     return (
-        <main className={styles.form_background} style={{background-image: image}}>
-            <div className={styles.form_text}>
+        <main className={styles.form_background} style={{backgroundImage: `url(${image})`}} onClick={() => {window.open(link, 'blank', 'noopener,noreferrer')}}>
+            <h1 className={styles.form_text}>
                 {text}
-            </div>
+            </h1>
         </main>
     )
 }
@@ -15,7 +16,9 @@ export default function FormsSection() {
     return (
         <main className={styles.forms_section}>
             <div className={styles.wrapper}>
-                <Form text={"hi"}/>
+                <Form text={"Intake Form"} image={'./images/FormsSection/intake_form.svg'} link={'https://roblox.com'}/>
+                <Form text={"Mentor Preference"} image={'./images/FormsSection/mentor_preference.svg'} link={'https://roblox.com'}/>
+                <Form text={"Mentor Applications"} image={'./images/FormsSection/mentor_applications.svg'} link={'https://roblox.com'}/>
             </div>
         </main>
     )

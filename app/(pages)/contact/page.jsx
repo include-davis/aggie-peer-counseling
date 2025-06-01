b1f28d995716beb91659ea050c064d92156602a1
austin1im <alaustinall@gmail.com>
2025-06-01 00:22:02 -0700

    created envelope

--- app/(pages)/contact/page.jsx
+++ app/(pages)/contact/page.jsx
@@ -1,10 +1,12 @@
 import styles from "./page.module.scss";
+import Envelope from "../_components/contact/Envelope/Envelope";
 
 export default function Home() {
   return (
     <main className={styles.page}>
       <h1>header 1</h1>
       <p>body</p>
+      <Envelope/>
     </main>
   );
 }

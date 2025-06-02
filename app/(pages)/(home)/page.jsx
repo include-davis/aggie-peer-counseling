7f6fb8a34a468774e5b77b1ec1f405b85d128a45
Sreya Mathew <ssmathew@ucdavis.edu>
2025-06-02 16:31:53 -0700

    add EmergencyContact and WhatWeDo to homepage

--- app/(pages)/(home)/page.jsx
+++ app/(pages)/(home)/page.jsx
@@ -1,6 +1,8 @@
 import styles from "./page.module.scss";
 import Hero from "../_components/common/Hero/Hero";
 import Program from "../_components/home/Program/Program";
+import EmergencyContact from "../_components/home/EmergencyContact/EmergencyContact";
+import WhatWeDo from "../_components/home/WhatWeDo/WhatWeDo";
 
 export default function Home() {
   return (
@@ -9,6 +11,8 @@ export default function Home() {
         title="Aggie Peer Counseling"
         subtitle="Here for you, Rain or Shine"
       />
+      <EmergencyContact />
+      <WhatWeDo />
       <Program />
       <h1>home</h1>
       <p></p>
